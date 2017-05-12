5bc588be935a0224a0cd8db41ec984fad3933cec
daniele.maddaluno <daniele.maddaluno@gmail.com>
2017-05-12 14:41:35 +0200

    chameleon arquillian

--- mybatis-ejb/src/test/java/com/madx/mybatis/TimeTest4.java
+++ mybatis-ejb/src/test/java/com/madx/mybatis/TimeTest4.java
@@ -6,7 +6,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -24,12 +24,21 @@ public class TimeTest4 {
 //            .addAsResource("META-INF/beans.xml")
 //            .addAsManifestResource(EmptyAsset.INSTANCE, "META-INF/beans.xml");
 //    }
-
-	@Deployment
-    public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, "test.war")
+//
+//	@Deployment
+//    public static WebArchive createDeployment() {
+//        return ShrinkWrap.create(WebArchive.class, "test.war")
+//            .addPackage(TimeMapper.class.getPackage())
+//            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+//    }
+	
+	// This static method builds the virtual test deployment archive
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(TimeMapper.class)
             .addPackage(TimeMapper.class.getPackage())
-            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 	
 	@Inject
