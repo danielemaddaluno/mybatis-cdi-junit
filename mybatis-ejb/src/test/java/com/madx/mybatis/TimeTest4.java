3caf5078b55b50e10abe9f3e033e60b7e4dc4602
daniele.maddaluno <daniele.maddaluno@gmail.com>
2017-05-12 12:50:27 +0200

    Arquillian test

---
+++ mybatis-ejb/src/test/java/com/madx/mybatis/TimeTest4.java
@@ -0,0 +1,42 @@
+package com.madx.mybatis;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.madx.mybatis.data.mappers.TimeMapper;
+
+
+@RunWith(Arquillian.class)
+public class TimeTest4 {
+
+//	// This static method builds the virtual test deployment archive
+//    @Deployment
+//    public static JavaArchive createDeployment() {
+//        return ShrinkWrap.create(JavaArchive.class)
+//            .addClass(TimeMapper.class)
+//            .addAsResource("META-INF/beans.xml")
+//            .addAsManifestResource(EmptyAsset.INSTANCE, "META-INF/beans.xml");
+//    }
+
+	@Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+            .addPackage(TimeMapper.class.getPackage())
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+	
+	@Inject
+	TimeMapper time;
+	
+	@Test
+	public void testNow(){
+		System.out.println(time.now());
+	}
+}
